13aea20d05c1e47d4e70cf92250b87abc7614890
Ramiro NAJERA <ramironajera@proton.me>
2026-05-18 12:09:11 +0200

    First commit

---
+++ plib_entry_panel.c
@@ -0,0 +1,9 @@
+/**
+ * @file plib_entry_panel.c
+ * @brief Widgets pour panneau en C (uniquement logique)
+ * @author Ramiro Najera
+ * @version 1.0.0
+ * @date 2026-05-18
+ */
+
+#include "plib_entry_panel.h"
---
+++ plib_entry_panel.h
@@ -0,0 +1,12 @@
+#ifndef PLIB_ENTRY_PANEL_H
+#define PLIB_ENTRY_PANEL_H
+
+/**
+ * @file plib_entry_panel.h
+ * @brief Widgets pour panneau en C (uniquement logique)
+ * @author Ramiro Najera
+ * @version 1.0.0
+ * @date 2026-05-18
+ */
+
+#endif  // PLIB_ENTRY_PANEL_H
